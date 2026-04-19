c67e0a8302edafcd2749cca753a7cb29a7e2d7ae
Renaat Debleu <rdebleu@eWallah.net>
2026-04-19 17:02:02 +0000

    502

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -29,6 +29,8 @@ use core_payment\privacy\paygw_provider;
 use core_privacy\local\request\{contextlist, approved_contextlist, userlist, approved_userlist, core_userlist_provider};
 use core_privacy\local\request\{writer, transform, deletion_criteria};
 use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\provider as metadata_provider;
+use core_privacy\local\request\plugin\provider as plugin_provider;
 use stdClass;
 
 /**
@@ -39,13 +41,8 @@ use stdClass;
  * @author     Renaat Debleu <info@eWallah.net>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\provider, \core_privacy\local\request\plugin\provider, core_userlist_provider, paygw_provider {
-    /**
-     * Returns meta data about this system.
-     *
-     * @param   collection $collection The initialised collection to add items to.
-     * @return  collection     A listing of user data stored through this system.
-     */
+class provider implements core_userlist_provider, metadata_provider, paygw_provider, plugin_provider {
+    #[\Override]
     public static function get_metadata(collection $collection): collection {
         $arr = [
             'userid' => 'privacy:metadata:paygw_airtelafrica:userid',
@@ -59,12 +56,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         return $collection;
     }
 
-    /**
-     * Get the list of contexts that contain user information for the specified user.
-     *
-     * @param int $userid The user to search.
-     * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
-     */
+    #[\Override]
     public static function get_contexts_for_userid(int $userid): contextlist {
         $sql = "SELECT id
                   FROM {context}
@@ -75,11 +67,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         return $contextlist;
     }
 
-    /**
-     * Export all user data for the specified user, in the specified contexts.
-     *
-     * @param approved_contextlist $contextlist The approved contexts to export information for.
-     */
+    #[\Override]
     public static function export_user_data(approved_contextlist $contextlist): void {
         global $DB;
         $contexts = $contextlist->get_contexts();
@@ -98,12 +86,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         }
     }
 
-    /**
-     * Get the list of users who have data within a context.
-     *
-     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
-     *
-     */
+    #[\Override]
     public static function get_users_in_context(userlist $userlist): void {
         $context = $userlist->get_context();
         if (!is_a($context, \context_user::class)) {
@@ -117,11 +100,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         $userlist->add_from_sql('userid', $sql, $params);
     }
 
-    /**
-     * Delete all data for all users in the specified context.
-     *
-     * @param context $context The specific context to delete data for.
-     */
+    #[\Override]
     public static function delete_data_for_all_users_in_context(\context $context): void {
         global $DB;
         if (is_a($context, \context_user::class)) {
@@ -129,23 +108,14 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         }
     }
 
-    /**
-     * Delete all user data for the specified user, in the specified contexts.
-     *
-     * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
-     */
+    #[\Override]
     public static function delete_data_for_user(approved_contextlist $contextlist): void {
         global $DB;
         $user = $contextlist->get_user();
         $DB->delete_records('paygw_airtelafrica', ['userid' => $user->id]);
     }
 
-    /**
-     * Delete multiple users within a single context.
-     *
-     * @param approved_userlist $userlist The approved context and user information to delete information for.
-     *
-     */
+    #[\Override]
     public static function delete_data_for_users(approved_userlist $userlist): void {
         global $DB;
         $context = $userlist->get_context();
@@ -155,13 +125,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         }
     }
 
-    /**
-     * Export all user data for the specified payment record, and the given context.
-     *
-     * @param \context $context Context
-     * @param array $subcontext The location within the current context that the payment data belongs
-     * @param \stdClass $payment The payment record
-     */
+    #[\Override]
     public static function export_payment_data(\context $context, array $subcontext, \stdClass $payment): void {
         global $DB;
         $subcontext[] = get_string('gatewayname', 'paygw_airtelafrica');
@@ -174,12 +138,7 @@ class provider implements \core_privacy\local\metadata\provider, \core_privacy\l
         }
     }
 
-    /**
-     * Delete all user data related to the given payments.
-     *
-     * @param string $paymentsql SQL query that selects payment.id field for the payments
-     * @param array $paymentparams Array of parameters for $paymentsql
-     */
+    #[\Override]
     public static function delete_data_for_payment_sql(string $paymentsql, array $paymentparams): void {
         global $DB;
         $DB->delete_records_select('paygw_airtelafrica', "paymentid IN ({$paymentsql})", $paymentparams);
